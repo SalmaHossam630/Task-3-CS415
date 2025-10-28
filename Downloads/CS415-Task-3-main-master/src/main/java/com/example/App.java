a01fee6e9b6369548f9f17f00a6f1e665061c999
SalmaHossam630 <salmahossam630@gmail.com>
2025-10-28 14:31:26 +0300

    Add Maven project back to repository

---
+++ Downloads/CS415-Task-3-main-master/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int multiply(int a,int b){return a*b;}
+}
